20615a3418832afe9c96e19c511ac17e77b368cd
Jason Son <jasonson2004@gmail.com>
2025-01-27 16:35:18 -0800

    hotfix(main)

--- src/main.js
+++ src/main.js
@@ -20,4 +20,4 @@ let borderUISize = game.config.height / 15
 let borderPadding = borderUISize / 3
 
 // reserve keyboard bindings
-let keyFIRE, keyRESET, keyLEFT, keyRIGHT
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT, keyMOUSE, keyKEYBOARD
